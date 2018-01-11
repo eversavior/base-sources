bb72f5a90cc6264a35479ba5648ccef078790cf5
Konstantin Klementev <konstantink@playtika.com>
2018-01-11 20:58:42 +0300

    upf

--- src/StartUp.groovy
+++ src/StartUp.groovy
@@ -6,6 +6,7 @@ import game.data.virtualDataBase.DataLoader
 import game.data.virtualDataBase.VirtualDataBase
 import http.HttpConnectionSession
 import threading.SessionManager
+import yobit.YobitService
 import yobit.YobitSignGenerator
 
 def language = "default";
@@ -17,6 +18,9 @@ new TrustedURLLoader().init();
 //4C29D5217856BFFFD70F8EEB2A1D2639
 println(new YobitSignGenerator().generateSign("8e38ca2cd3c33351a87f8ba11c76d012", "nonce=2&method=getInfo"));
 println("3f867e6270830ad2f8afc9042a58111bcf0259259cac8a73b600d6ea9b9b2f2d8d16133d5b5f962be6a1041fb5078f48df0b516b7f6f21f1300f869185b48eaa");
+
+new YobitService()
+
 return;
 
 def dataBase = new VirtualDataBase();
